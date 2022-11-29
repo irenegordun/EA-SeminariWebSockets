2205977185956f50b67dc5a0d25362eb1a3de39f
irenegordun <irene.gordun@estudiantat.upc.edu>
2022-11-29 16:41:20 +0100

    user

--- chat-angular/src/app/services/chat.service.ts
+++ chat-angular/src/app/services/chat.service.ts
@@ -9,20 +9,20 @@ import { ChatComponent } from '../component/chat/chat.component';
 
 export class ChatService {
 
-  chats: { text: string; messageType: number; }[] = [];
+  chats: { text: string; messageType: number; user: string}[] = [];
 
 
   constructor(private socket:SocketService) {
     this.onReceiveMessage();
   }
 
-  sendMessage(messageInfo: { text: string; messageType: number; }){
+  sendMessage(messageInfo: { text: string; messageType: number; user :string }){
     this.chats.push(messageInfo);
     this.socket.io.emit("sendMessage",messageInfo);
   }
 
   onReceiveMessage(){
-    this.socket.io.on("receiveMessage", (messageInfo: { text: string; messageType: number; })=>{
+    this.socket.io.on("receiveMessage", (messageInfo: { text: string; messageType: number; user:string })=>{
       messageInfo.messageType=2;
       this.chats.push(messageInfo);
     }) 
